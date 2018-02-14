fb4f20b9f44e2cdf35d6b968691b7a874953c8b0
0LIN1ISA <0LIN1ISA@protonmail.com>
2018-02-15 01:27:17 +0800

    fix password

--- app/src/main/java/com/example/a0lin1isa/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/example/a0lin1isa/broadcastbestpractice/LoginActivity.java
@@ -21,15 +21,15 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        accountEdit = (EditText) findViewById(R.id.account);
-        passwordEdit = (EditText) findViewById(R.id.password);
-        login = (Button) findViewById(R.id.login);
+        accountEdit = findViewById(R.id.account);
+        passwordEdit = findViewById(R.id.password);
+        login = findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
-                if (account.equals("admin") && password.equals("081123")) {
+                if (account.equals("admin") && password.equals("123456")) {
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
